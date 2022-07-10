14a20d531ac224a7432be839e9b9f9093a00bcaa
Sumit Titare <sumittitare06@gmail.com>
2022-07-11 00:30:29 +0530

    Added Resourceful Routes& Controller Actions

--- assignment_1_simple_cms_blueprint/config/routes.rb
+++ assignment_1_simple_cms_blueprint/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+
+  root 'subjects#index'
+
+  resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
