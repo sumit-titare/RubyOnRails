80cfcf772e1741e9cac0e34ea7cf154f5521e814
Sumit Titare <sumittitare06@gmail.com>
2022-07-10 14:56:41 +0530

    Added Scopes for Models

--- assignment_1_simple_cms_blueprint/app/models/subject.rb
+++ assignment_1_simple_cms_blueprint/app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ApplicationRecord
+
+  scope :visible, lambda { where(visible: true) }
+  scope :invisible, lambda { where(visible: false) }
+  scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
+  scope :highest_position, lambda { order("position DESC").limit(1) }
+  scope :sorted, lambda { order('position ASC')}
+  scope :newest_first, lambda { order('created_at DESC') }
 end
